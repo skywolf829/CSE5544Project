b1b6c4f1c0186217edf0d6e4ffb1542131ba6365
Patrick Capps <patrick.capps0@gmail.com>
2019-03-31 14:35:46 -0400

    room actually bounds the user

---
+++ CSE5544Project/Assets/Scripts/StayInBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInBounds : MonoBehaviour
+{
+    float xUpper = 4.7f;
+    float xLower = -4.7f;
+    float zUpper = 3.7f;
+    float zLower = -1.7f;
+    private void Update()
+    {
+        if (transform.position.x >= xUpper)
+        {
+            transform.position = new Vector3(xUpper, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= xLower)
+        {
+            transform.position = new Vector3(xLower, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z >= zUpper)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zUpper);
+        }
+        if (transform.position.z <= zLower)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLower);
+        }
+    }
+}
